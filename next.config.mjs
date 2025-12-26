27c6e1ec50602b7a2959419bec47564824d34796
dharmaryan <117965050+dharmaryan@users.noreply.github.com>
2025-12-26 21:52:48 +1100

    Swap to @sparticuz/chromium for PDF

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ["playwright-core", "@sparticuz/chromium"],
+  },
   images: {
     remotePatterns: [
       {
